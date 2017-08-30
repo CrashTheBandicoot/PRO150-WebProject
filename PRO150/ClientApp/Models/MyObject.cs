ddcf6195a19cce81058f323bf3182e9692978069
klee <knlee6@gmail.com>
2017-08-30 09:10:03 -0600

    Can't push still breaks when I call the server.

---
+++ PRO150/ClientApp/Models/MyObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientApp.Models
+{
+    public class MyObject
+    {
+        public int gameId { get; set; }
+        public int playerId { get; set; }
+        public string error { get; set; }
+        public bool success { get; set; }
+    }
+}
---
+++ PRO150/ClientApp/Scripts/GameBoard.js
@@ -0,0 +1,26 @@
+﻿
+var canvas = document.getElementById("gameBoard");
+var ctx = canvas.getContext('2d');
+function makeBoard() {
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++){
+            if (((i == 0 || i == 2 || i == 4 || i == 6) && (j == 1 || j == 3 || j == 5 || j == 7)) || ((i == 1 || i == 3 || i == 5 || i == 7) && (j == 0 || j == 2 || j == 4 || j == 6))) {
+                drawSquare('b', i, j);
+            }
+            else {
+                drawSquare('r', i, j);
+            }
+        }
+    }
+}
+makeBoard();
+
+function drawSquare(color, x, y) {
+    if (color == 'r') {
+        ctx.fillStyle = "rgb(255, 0, 0)";
+    }
+    else {
+        ctx.fillStyle = "rgb(0, 0, 0)";
+    }
+    ctx.fillRect(x*50, y*50, 50, 50);
+}
